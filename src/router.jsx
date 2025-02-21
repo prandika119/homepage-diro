5fd5cd53b9a6676836e5fbe6a191670310fe88cd
prandika119 <an.prasetya9@gmail.com>
2025-02-21 21:06:49 +0700

    update-router

--- src/router.jsx
+++ src/router.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <Home />,
+                element: <HomePage />,
             },
             {
                 path: "/home",
